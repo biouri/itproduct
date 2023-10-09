0939546985f793694408972d8fa6ae52f1f61a16
Yury Belokhonov <belokhonov@gmail.com>
2023-10-09 03:00:26 +0300

    Create User in DB + Get tokens and password hash + Add jaguar_jwt Plugin

--- README.md
+++ README.md
@@ -11,22 +11,28 @@ msinfo32 - GUI tool
 ### Install WSL2 before Docker (only for Windows)
 https://learn.microsoft.com/en-us/windows/wsl/install
 
+```bash
 wsl --install
 reboot
+```
 
 Check WSL2 installation
+```bash
 wsl -l -v
-------------------------------------------------
-  NAME                   STATE           VERSION
-* Ubuntu                 Running         2
+```
+|   :----: | :----:  | ---     |
+|   NAME   | STATE   | VERSION |
+| * Ubuntu | Running | 2       |
 
 ### Docker Installation Process
 https://docs.docker.com/desktop/install/mac-install/
 https://docs.docker.com/desktop/install/windows-install/
 
 Check Docker Installation
+```bash
 docker --version
 docker compose version
+```
 
 ### VSCode
 https://code.visualstudio.com/
@@ -55,10 +61,14 @@ The image is downloaded once and can then be used repeatedly.
 You can use the same image for another application.
 
 Run in Terminal for the first time: 
+```bash
 docker compose up --build
+```
 
 Run in Terminal after build (in the background):
+```bash
 docker compose up -d
+```
 
 ## Installing Flutter, Dart SDK, Conduit for writing a backend
 
@@ -72,18 +82,24 @@ https://brew.sh/
 https://dart.dev/get-dart
 
 The Dart SDK installation (Windows example):
+```bash
 choco install dart-sdk
+```
 
 To upgrade the Dart SDK (Windows example):
+```bash
 choco upgrade dart-sdk
+```
 
 ### Flutter Install
 https://docs.flutter.dev/get-started/install
 
 Check Dart & Flutter Version
+```bash
 dart --version
 flutter --version
 where flutter dart
+```
 
 ### Framework Conduit Install
 https://www.theconduit.dev/
@@ -91,38 +107,46 @@ https://www.theconduit.dev/
 Get Started
 https://www.theconduit.dev/start/
 
+```bash
 dart pub global activate conduit
 conduit --version
+```
 Conduit CLI version: 4.4.0
 
 ### Create a new Dart project.
 https://dart.dev/tutorials/server/get-started#3-create-a-small-app
 
+```bash
 dart create -t console auth
+```
 
 Set Application entry point
 lib\main.dart
 
 Add dependencies to: 
 auth\pubspec.yaml
+```yaml
 ...
 dependencies:
   conduit: ^4.4.0
   conduit_core: ^4.4.0
   conduit_postgresql: ^4.4.0
+```
 
 From:
 https://pub.dev/packages/conduit/install
 https://pub.dev/packages/conduit_core/install
 https://pub.dev/packages/conduit_postgresql/install
 
-
 ### Getting packages
+```bash
 cd .\auth\
 dart pub get
+```
 
 ## Creating a migration file + Validation + Making changes to the DB
+```bash
 conduit db generate
 conduit db validate
 conduit db upgrade
-
+```
--- auth/lib/controllers/app_auth_controller.dart
+++ auth/lib/controllers/app_auth_controller.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:auth/models/app_response_model.dart';
 import 'package:auth/models/user.dart';
 import 'package:conduit_core/conduit_core.dart';
+import 'package:jaguar_jwt/jaguar_jwt.dart';
 
 class AppAuthController extends ResourceController {
   final ManagedContext managedContext;
@@ -46,22 +48,41 @@ class AppAuthController extends ResourceController {
       ));
     }
 
-    final User fetchedUser = User();
+    final salt = generateRandomSalt();
+    final hashPassword = generatePasswordHash(user.password ?? "", salt);
 
     // connect to DB
     // create user
     // fetch user
 
-    return Response.ok(
-      AppResponseModel(
-        data: {
-          "id": fetchedUser.id,
-          "refreshToken": fetchedUser.refreshToken,
-          "accessToken": fetchedUser.accessToken, 
-        },
-        message: "Успешная регистрация"
-      ).toJson(),
-    );
+    try {
+      late final int id;
+      await managedContext.transaction((transaction) async {
+        final qCreateUser = Query<User>(transaction)
+          ..values.username = user.username
+          ..values.email = user.email
+          ..values.salt = salt
+          ..values.hashPassword = hashPassword;
+        final createdUser = await qCreateUser.insert();
+        id = createdUser.asMap()["id"];
+        final Map<String, dynamic> tokens = _getTokens(id);
+        final qUpdateTokens = Query<User>(transaction)
+          ..where((user) => user.id).equalTo(id)
+          ..values.accessToken = tokens["access"]
+          ..values.refreshToken = tokens["refresh"];
+        await qUpdateTokens.updateOne();
+      });
+      final userData = await managedContext.fetchObjectWithID<User>(id);
+      return Response.ok(
+        AppResponseModel(
+          data: userData?.backing.contents,
+          message: "Успешная регистрация"
+        )
+      );
+    }
+    on QueryException catch(error) {
+      return Response.serverError(body: AppResponseModel(message: error.message));
+    }
   }
 
   // Обновление токена
@@ -84,5 +105,22 @@ class AppAuthController extends ResourceController {
         message: "Успешное обновление токенов"
       ).toJson(),
     );
+  }
+  
+  Map<String, dynamic> _getTokens(int id) {
+    // TODO remove when release
+    final key = Platform.environment["SECRET_KEY"] ?? "SECRET_KEY";
+
+    final accessClaimSet = JwtClaim(
+      maxAge: Duration(hours: 1), 
+      otherClaims: {"id": id});
+
+    final refreshClaimSet = JwtClaim(
+      otherClaims: {"id": id});
+
+    final tokens = <String, dynamic>{};
+    tokens["access"] = issueJwtHS256(accessClaimSet, key);
+    tokens["refresh"] = issueJwtHS256(refreshClaimSet, key);
+    return tokens;
   }
 }
